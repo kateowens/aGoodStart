c4abaa0f9edf68c9ac87a6811e30f8229e7d51ab
Kate Owens <kowens711@gmail.com>
2017-04-10 10:18:42 -0400

    KO - added code to git repo

---
+++ AGoodStart/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Katherine Owens on 4/6/17.
+//  Copyright © 2017 Katherine Owens. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    //Print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    /* Is that actually any good? 
+     Maybe it needs a rewrite.*/
+    
+    return 0;
+}
+
+float weight;
